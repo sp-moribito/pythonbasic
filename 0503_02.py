84960359064f7fbec10690069a8e82f1ae50e808
sp-moribito <79080501+sp-moribito@users.noreply.github.com>
2021-05-03 17:34:59 +0900

    Add files via upload

---
+++ 0503_02.py
@@ -0,0 +1,14 @@
+import time
+import datetime
+
+now=datetime.datetime.now()
+mid=datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+
+while True:
+    now=datetime.datetime.now()
+    if now==mid:
+        print("at time")
+        now=datetime.datetime.now()
+        mid=datetime.datetime(now.year, now.month, now.day) + datetime.timedelta(1)
+    print(now,"vs",mid)
+    time.sleep(1)
